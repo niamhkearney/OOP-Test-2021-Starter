19431fbf0a7e91c6090c3aee6268dc18ecdf509e
niamhkearney <niamhnichearnaigh@gmail.com>
2021-04-21 20:40:16 +0100

    drew a note

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -14,32 +14,33 @@ public class ScoreDisplay extends PApplet {
 	ArrayList<Note> note = new ArrayList<Note>();
 
 	public void loadScore() {
-
+		// this variable holds a character from the string until a number is found and
+		// if it doesn't it empties itself
 		char hold = 0;
 
-		for (int k = note.size() - 1; k >= 0; k--) {
-			for (int i = 0; i < score.length(); i++) {
-				String s = score.substring(i, i + 1);
+		for (int i = 0; i < score.length(); i++) {
+			String s = score.substring(i, i + 1);
 
-				int ind = score.indexOf(s);
+			int ind = score.indexOf(s);
 
-				// converting the substring to a character
-				char ch = score.charAt(ind);
+			// converting the substring to a character
+			char ch = score.charAt(ind);
 
-				// checking is the character is a digit or not
-				if (Character.isDigit(ch) == true) {
+			// checking is the character is a digit or not
+			if (Character.isDigit(ch) == true) {
 
-					int num = ch - '0';
-					hold = 0;
-				} else if (Character.isDigit(ch) == false && hold != 0) {
-					// note.add();
-				} else {
-
-				}
-
-				// System.out.println(s);
+				int num = ch - '0';
+				hold = 0;
+			} else if (Character.isDigit(ch) == false && hold != 0) {
+				// note.add(hold, 1);
+				// add character and 1 into the arraylist
+			} else {
+				hold = ch;
 			}
+
+			System.out.println(s);
 		}
+
 	}
 
 	public void settings() {
@@ -65,8 +66,14 @@ public class ScoreDisplay extends PApplet {
 			line(100, i, 1000, i);
 		}
 
+		strokeWeight(2);
+		fill(0);
+		circle(100, 250, 20);
+		line(110, 250, 110, 210);
+
 	}
 
 	void drawNotes() {
+
 	}
 }

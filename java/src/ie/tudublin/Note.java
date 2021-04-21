4360bf0547fcd67a6d07839fff6258c3621cff3b
niamhkearney <niamhnichearnaigh@gmail.com>
2021-04-21 16:52:07 +0100

    setters and getters for note and duration

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -9,4 +9,20 @@ public class Note {
     private char note;
     private int duration;
 
+    public char getNote() {
+        return note;
+    }
+
+    public void setNote(char note) {
+        this.note = note;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration() {
+        this.duration = duration;
+    }
+
 }
